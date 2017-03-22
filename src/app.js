e8e7f60ef8e878e2f2a9e15d2c3c56a2ed2c4930
Jordi Aguilar <jaguilar@nicepeopleatwork.com>
2017-03-22 11:04:10 +0100

    improved html doc routes

--- src/app.js
+++ src/app.js
@@ -1,9 +1,13 @@
 var server = require('./server')
 
+global.dev = (process.env.NODE_ENV === 'development')
+global.port = process.env.port || 8080
+global.baseurl = global.dev ? `http://localhost:${global.port}/` : 'http://pejotas.klerix.com/api/'
+
 // router
 require('./router')(server)
 
 // Start listening
-server.listen(8080, () => {
+server.listen(global.port, () => {
   console.log(`${server.name} listening at ${server.url}`)
 })
